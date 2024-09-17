f670f88d9fb6224734278192984fedd14675d4fa
grishhung <justinhung@gmail.com>
2024-09-16 21:31:25 -0700

    Correct erroneous comment

--- constants.py
+++ constants.py
@@ -5,13 +5,12 @@ GLOBAL_COEFF = 1.4
 CURVE_LOG_BASE = 1.5
 CURVE_LEN_COEFF = 5
 
-# Chosen arbitrarily; subject to change
+# Based on whether a strum is required
 FORCING_TO_RH_ACTIONS: Dict[Forcing, int] = {
     Forcing.STRUM: 1,
     Forcing.HOPO: 0,
     Forcing.TAP: 0,
 }
-
 
 # Based roughly on subsequent multiplications by 0.75 
 DIFF_TO_ROCK_METER_SIZE: Dict[Diff, int] = {
